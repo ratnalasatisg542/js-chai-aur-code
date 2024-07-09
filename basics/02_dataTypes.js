9f128cc7ce5e730a85faedd88fe48f44ec4658b9
Ratnala Satish <ratnalasatish542@gmail.com>
2024-07-09 15:23:37 +0000

    dataTypes Done

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+//alert( 3 + 3) //we are using nodejs not browser
+
+console.log(2 + 2 );
+
+
+let name = "satish"
+let age = 20
+let isloggedin = true
+
+//string =>""
+//boolean = true/false
+//undefined = variable assigned but we havent assigned and value to it
+//null => is a representation of empty value // standalone value
+//symbol=> unique
+
+
+//object
+console.log(typeof age);
+console.log(typeof null); //objects
+console.log(typeof undefined); //undefined
