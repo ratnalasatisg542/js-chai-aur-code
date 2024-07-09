78773a2522f5d701aac024e02cd33f4371afa62a
Ratnala Satish <ratnalasatish542@gmail.com>
2024-07-09 15:37:48 +0000

    conversion and operation covered

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+/*conversion and operations*/
+let score = 40
+let Name = "satish"
+
+console.log(typeof (score));
+console.log( typeof(Name) );
+
+let valueInNumber = Number(Name)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
