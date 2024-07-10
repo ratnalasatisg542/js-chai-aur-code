22ad33539ae7167eb7f0683b60f323c36cca15e2
Ratnala Satish <ratnalasatish542@gmail.com>
2024-07-10 17:34:01 +0000

    conversion datatype

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -1,10 +1,39 @@
 /*conversion and operations*/
-let score = 40
+/*let score = 40
 let Name = "satish"
 
 console.log(typeof (score));
-console.log( typeof(Name) );
+console.log( typeof(Name) );*/
 
-let valueInNumber = Number(Name)
+
+
+/*let valueInNumber = Number(Name)
 console.log(typeof valueInNumber);
-console.log(valueInNumber);
+console.log(valueInNumber);*/
+
+
+/*when your converting 30 => it's type is number
+  when your converting 30abc => it's type is NAN
+  when your converting NULL => it's type  is 0
+  when your converting undefined => it's type  is NAN
+  when your converting true => it's type  is 1
+  when your converting false => it's type  is 0 */
+
+
+
+ // let isLoggedIn = 1
+
+  //let booleanisLogged = Boolean(isLoggedIn)
+  //console.log(booleanisLogged);
+  //console.log(typeof booleanisLogged);
+  
+
+  //1 => true, 0=> false 
+  //"" => false
+  //"abcd"=> true
+
+  let xnumber = 22
+
+  let somestring = String (xnumber)
+  console.log(somestring);
+  console.log(typeof somestring);
