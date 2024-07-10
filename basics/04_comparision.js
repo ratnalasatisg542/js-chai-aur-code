38a3004f9e97954a61b2ba0053a92079ddc70df9
Ratnala Satish <ratnalasatish542@gmail.com>
2024-07-10 18:58:09 +0000

    comparision

---
+++ basics/04_comparision.js
@@ -0,0 +1,22 @@
+//  console.log(2 > 10);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("0" > 1);
+// console.log("02" > 1);
+// by defalt string converted to  number
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === 
+// we are comparing data type in this  in this they both are equal but not eaqual in data type wise
+console.log("2" === 2);
