194f7e50e1d26484c2625f222305e8bc8b7ffc0a
Ratnala Satish <ratnalasatish542@gmail.com>
2024-07-21 18:13:04 +0000

    arrays

--- 02Basics/Arrays.js
+++ 02Basics/Arrays.js
@@ -43,3 +43,18 @@ console.log(mynew2);
 
 // //slice() does not manupulate main array.
 // => splice() manupulate original array
+
+
+
+const Heros = [ "Hulk  ", "spiderman" , "black panter", "tieo","jack"]
+
+// console.log(Heros.slice(1,3));
+// console.log(Heros.slice(2)); // its the start value and it will take till the end 
+
+console.log(Heros.splice(1,3)); // 1 is the start and 3 is not the end point its the range from 1 we count 3 steps forword in this case spdy is 1 and count 3 steps including spdy 1 = spydy 2 blackpanter 3 is tieo thats how you get the output (spdy,bkp,tieo)
+
+
+// Heros.splice(1,3,"hii")
+// console.log(Heros);   // in this we can replace with hi 
+ 
+console.log(Heros.splice (1,3,));
