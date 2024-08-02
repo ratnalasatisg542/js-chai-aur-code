9e5e6902ea293b19c9136371169fa028355d464a
Ratnala Satish <ratnalasatish542@gmail.com>
2024-08-02 11:59:28 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,68 @@
+
+
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("jan");
+        
+//         break;
+//     case 2:
+//         console.log("feb");
+        
+//         break;
+//     case 3:
+//         console.log("march");
+        
+//         break;
+//     case 4:
+//         console.log("april");
+        
+//         break;
+   
+//     default:
+//         console.log("default case");
+        
+//         break;
+// }
+
+
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        
+        break;
+    case "feb":
+        console.log("febuary");
+        
+        break;
+    case "march":
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
+   
+    default:
+        console.log("default case");
+        
+        break;
+}
