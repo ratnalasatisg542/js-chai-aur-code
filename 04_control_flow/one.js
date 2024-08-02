7d603b171786e8ef168b9ae40c4b8e8cbb74aee4
Ratnala Satish <ratnalasatish542@gmail.com>
2024-08-02 11:59:05 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,101 @@
+//if
+
+// if (condition) {
+
+// }
+
+// const isUserLoggedin = true
+
+// if (3!=2) {
+//     console.log("sucessful");
+    
+
+// }
+
+
+// if (2==="2") {
+//     console.log("executed");
+    
+
+// }
+
+// if (2==2){
+
+// }
+// >,<,<=,>=,==,!=,===
+
+
+
+// const temprature = 59
+
+// if (temprature <50) {
+    
+//     console.log("less than 50");
+// }
+// else {
+//     console.log("more tham 50");
+    
+// }
+// console.log("executed");
+
+
+
+const score = 200;
+
+if (score>100) 
+{
+    let power = "fly"
+    console.log(`user can ${power}`);
+    
+}
+// console.log(`${power}`); power is out of scope 
+
+
+// const balance = 1000
+// if (balance > 500) console.log("test"); implesit scope
+
+    // if (balance < 500) {
+        
+    //     console.log("less than 1k");
+        
+    // }
+    // else if (balance < 750 ) {
+    //     console.log("less than 750");
+        
+    // }
+
+    // else if (balance < 900) {
+    //     console.log("less than 900");
+        
+    // }
+
+    // else {
+    //     console.log(" less than 12000 ");
+        
+    // }
+
+
+
+    // const userLoggedIn = true
+    // const debitCard = true
+    // const loggedInFromGoogle = false
+    // const LoggedinByEmail = true
+
+    // if (userLoggedIn && debitCard && 2==3) {
+    //     console.log("allow this user to buy course");
+        
+    // }
+    // else {
+    //     console.log("plz log in amd use debit card");
+        
+    // }
+
+    // if (loggedInFromGoogle || LoggedinByEmail ) {
+
+    //     console.log("user LoggedIn");
+        
+    // }
+
+
+
+    
