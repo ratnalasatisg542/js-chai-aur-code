18f6f49b0386502e270d838743f01e4bd2b1771e
Ratnala Satish <ratnalasatish542@gmail.com>
2024-08-03 11:10:20 +0000

    while do while

---
+++ 05_loops.js/two.js
@@ -0,0 +1,26 @@
+// while & do while loop
+
+
+
+let index = 0
+while (index <=10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let MyArray = ["flash", 'ironman', "batman"]
+let arr = 0
+while (arr < MyArray.length) {
+    // console.log(`superheros ${MyArray[arr]}`);
+    arr = arr +1 
+}
+
+let score = 11
+do {
+     console.log(`score is ${score}`);
+     score ++
+} while (score <10) {
+    
+}
+
+
