3a1f080d31bca9c464cf0dede557c7b8bd5c20d4
Ratnala Satish <ratnalasatish542@gmail.com>
2024-08-07 17:33:59 +0000

    six

--- 05_loops.js/six.js
+++ 05_loops.js/six.js
@@ -45,7 +45,7 @@ const books = [
     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
   ];
 
-   const userBooks = books.filter( (bk) => bk.genre === 'History')
+   let userBooks = books.filter( (bk) => bk.genre === 'History')
   
 //   userBooks = books.filter( (bk) => { 
 //     return bk.publish >= 1995 && bk.genre === "History"
