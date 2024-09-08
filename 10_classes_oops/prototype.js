3f31b3cd11ca7e91a498ae1635d1d911d09bfa77
Ratnala Satish <ratnalasatish542@gmail.com>
2024-09-08 16:36:54 +0000

    proto

---
+++ 10_classes_oops/prototype.js
@@ -0,0 +1,17 @@
+let myName = "satish     ";
+let myNamechannel = "satish     ";
+
+console.log(myName.truelength());
+
+
+let myHeros = ['THOR', 'SPIDERMAN', ]
+
+
+let heroPower = {
+     THOR : "hammer",
+     SPIDERMAN : "sling" ,
+     getspidyPower: function (){
+        console.log(`spider power is ${this.SPIDERMAN}`);
+        
+     }
+}
